3f1ba58273f35a724768ed4ac2c03192b4cfac37
devarturmartins <arturvl1@hotmail.com>
2023-01-23 16:59:59 -0300

    Req. 1 - Feito a requisição API e colocado os itens da response na tabela

--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -1,14 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import PropTypes from 'prop-types';
-import { useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import AppContext from './AppContext';
 import useFetch from '../hooks/useFetch';
 
 function AppProvider({ children }) {
+  const [dataPlanet, setDataPlanet] = useState([]);
   const { errors, isLoading, makeFetch } = useFetch();
+  useEffect(() => {
+    const responseApi = async (url) => {
+      const resposta = await makeFetch(url);
+      resposta.results.forEach((e) => delete e.residents);
+      setDataPlanet(resposta);
+    };
+    responseApi('https://swapi.dev/api/planets');
+  }, []);
   const values = useMemo(() => ({
-    isLoading, errors, makeFetch,
-  }), [isLoading, errors]);
+    isLoading, errors, makeFetch, dataPlanet,
+  }), [isLoading, errors, dataPlanet]);
   return (
     <AppContext.Provider value={ values }>
       { children }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,55 @@
 import React, { useContext } from 'react';
-import appContext from '../context/AppContext';
+import AppContext from '../context/AppContext';
 
 function Home() {
-  const { isLoading, errors, makeFetch } = useContext(appContext);
-
-  useEffect(() => {
-    const responseApi = async (url) => {
-      const response = await makeFetch(url);
-      return response;
-    };
-    responseApi('https://swapi.dev/api/planets');
-    console.log(responseApi('https://swapi.dev/api/planets'));
-  }, []);
+  const { isLoading, dataPlanet } = useContext(AppContext);
+  const { results } = dataPlanet;
   return (
     <div>
-      home
+      { isLoading
+        ? (<p>Carregando...</p>)
+        : (
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Rotation Period</th>
+                <th>Orbital Period</th>
+                <th>Diameter</th>
+                <th>Climate</th>
+                <th>Gravity</th>
+                <th>Terrain</th>
+                <th>Surface Water</th>
+                <th>Population</th>
+                <th>Films</th>
+                <th>Created</th>
+                <th>Edited</th>
+                <th>URL</th>
+              </tr>
+            </thead>
+            {
+              results?.map((e, i) => (
+                <tbody key={ i }>
+                  <tr>
+                    <td>{ e.name }</td>
+                    <td>{ e.rotation_period }</td>
+                    <td>{ e.orbital_period }</td>
+                    <td>{ e.diameter }</td>
+                    <td>{ e.climate }</td>
+                    <td>{ e.gravity }</td>
+                    <td>{ e.terrain }</td>
+                    <td>{ e.surface_water }</td>
+                    <td>{ e.population }</td>
+                    <td>{ e.films }</td>
+                    <td>{ e.created }</td>
+                    <td>{ e.edited }</td>
+                    <td>{ e.url }</td>
+                  </tr>
+                </tbody>
+              ))
+            }
+          </table>
+        )}
     </div>
   );
 }
